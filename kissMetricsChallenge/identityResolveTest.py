5608b6aeb20f18f29315c551c9b7be2847873760
Vikas Agartha <vikasagartha@gmail.com>
2015-05-04 08:56:31 -0500

    Refactored tests completeley. Created a dynamic test that covers the goals of previous tests and more. Deleted the unnecessary tests, and tests that were specific to one database (ie, searching for 'foo', which I knew to exist in my test database 'people.db'). Also added some comments to the tests.

--- kissMetricsChallenge/identityResolveTest.py
+++ kissMetricsChallenge/identityResolveTest.py
@@ -7,64 +7,40 @@ DBNAME = 'people.db'
 
 class TestDDB(unittest.TestCase):
 
-    def checkFirstVal(self, person, db, key):
-            firstValue = (db[key])[1:]
-            return (firstValue in person.anonIds) or (firstValue == person.internalId)
-
     def testLittleEndianConversion(self):
         self.assertEquals(int(0x0d0c0b0a), convertToUnicode('\x0a\x0b\x0c\x0d'))
 
-    def testFooAnonId(self):
-        key = 'foo'
-        db = pytc.BDB(DBNAME, pytc.BDBOREADER)
-        p = buildPerson(db, key, Person(key))
-        anonValue = db['foo'] 
-        self.assertEquals(p.anonIds[0], anonValue[1:])
-
-    def test10RandomKeys(self):
+    #identityResolve tool uses recursive calls to build a Person object.
+    #This test matches the process iteratively. 
+    def test10RandomKeyDynamically(self):
         db = pytc.BDB(DBNAME, pytc.BDBOREADER)
         keys = []
         for key in db.iterkeys():
             keys.append(key)
-        num = 0
-        while num<10:
+
+        testnum = 0
+        while testnum<10:
             randomKey = random.choice(keys)
             p = buildPerson(db, randomKey, Person(randomKey))
-            testResult = self.checkFirstVal(p,db,randomKey)
-            self.assertEquals(True, testResult)
-            num+=1
+            
+            key = randomKey
+            value = ""
+            while(1):
+                value = db[key]
 
-    def testFirst10Keys(self):
-        db = pytc.BDB(DBNAME, pytc.BDBOREADER)
-        num = 0
-        for key in db.iterkeys():
-            if num>9:
-                break
-            p = buildPerson(db, key, Person(key))
-            testResult = self.checkFirstVal(p, db, key)
-            self.assertEquals(True, testResult)
-            num+=1
+                if value[0] == 'n':
+                    self.assertEquals(value[1:] in p.anonIds, True)
+                    key = value[1:]
 
-    def test10AnonIdSplicing(self):
-        db = pytc.BDB(DBNAME, pytc.BDBOREADER)
-        num = 0
-        for key in db.iterkeys():
-            if num>9:
-                break
-            num+=1
-            p = buildPerson(db, key, Person(key))
-            for anonId in p.anonIds:
-                self.assertNotEquals('n', anonId[0])
+                elif value[0] == 'i':
+                    break
 
-    def test10internalIdSplicing(self):
-        db = pytc.BDB(DBNAME, pytc.BDBOREADER)
-        num = 0
-        for key in db.iterkeys():
-            if num>9:
-                break
-            num+=1
-            p = buildPerson(db, key, Person(key))
-            self.assertNotEquals('i', p.internalId[0])
+                else:
+                    print 'Found an identifier value starting with neither \"n\" nor \"i\"\n'
+                    break
+            
+            self.assertEquals(value[1:], p.internalId)
+            testnum += 1
 
 if __name__ == '__main__':
         unittest.main()
